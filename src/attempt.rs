e84ceee047e9257e39913c53a2f52bf2d325591b
gabref <gabrielfranzer@gmail.com>
2023-12-15 13:21:00 +0100

    removing unnecesary stuff

--- src/attempt.rs
+++ src/attempt.rs
@@ -1,4 +1,3 @@
-// Define a trait for the common functionalities of Pix products
 trait CreditCardSupplier {
     fn init(&mut self, config: &str, property: &str) -> String {
         let f = self.from_input_init(config, property);
@@ -18,47 +17,42 @@ trait CreditCardSupplier {
     fn from_input_init(&self, config: &str, property: &str) -> Box<dyn SubProductApi>;
 }
 
-// Define the trait for the sub-product API
 trait SubProductApi {}
 
-// Implement the trait for FastCash API
 struct SupplierOneApi {
-    fastcash_property: Option<String>,
-    fastcash_configs: Option<String>,
+    supplier_one_property: Option<String>,
+    supplier_one_configs: Option<String>,
 }
 impl SupplierOneApi {
     fn get_supplier_one_property(&self) -> String {
-        self.fastcash_property.clone().unwrap_or_default()
+        self.supplier_one_property.clone().unwrap_or_default()
     }
     fn set_supplier_one_configs(&mut self, c: String) {
-        self.fastcash_configs = Some(c);
+        self.supplier_one_configs = Some(c);
     }
 }
 
 impl SubProductApi for SupplierOneApi {}
 
-// Implement the trait for Matera API
 struct SupplierTwoApi {
-    matera_property: Option<String>,
-    matera_configs: Option<String>,
+    supplier_two_property: Option<String>,
+    supplier_two_configs: Option<String>,
 }
 impl SupplierTwoApi {
     fn get_supplier_two_property(&self) -> String {
-        self.matera_property.clone().unwrap_or_default()
+        self.supplier_two_property.clone().unwrap_or_default()
     }
     fn set_supplier_two_configs(&mut self, c: String) {
-        self.matera_configs = Some(c);
+        self.supplier_two_configs = Some(c);
     }
 }
 
 impl SubProductApi for SupplierTwoApi {}
 
-// Define the Pix struct with a generic type parameter
 struct CreditCard {
     selected_product: Option<Box<dyn CreditCardSupplier>>,
 }
 
-// Implement methods for Pix
 impl CreditCard {
     fn new() -> Self {
         CreditCard {
@@ -67,7 +61,6 @@ impl CreditCard {
     }
 
     fn set(&mut self, product_name: &str) {
-        // Initialize the selected product based on the product name
         match product_name {
             "SupplierOne" => {
                 self.selected_product = Some(Box::new(SupplierOne {
@@ -84,14 +77,12 @@ impl CreditCard {
     }
 
     fn init_product(&mut self, config: &str, property: &str) {
-        // Call the init method on the selected product
         if let Some(ref mut selected_product) = self.selected_product {
             selected_product.init(config, property);
         }
     }
 
     fn process_product(&self, command: &str) -> String {
-        // Call the process method on the selected product
         if let Some(ref selected_product) = self.selected_product {
             selected_product.process(command)
         } else {
@@ -100,7 +91,6 @@ impl CreditCard {
     }
 }
 
-// Implement PixProduct for FastCash
 struct SupplierOne {
     sub_product_api: Option<SupplierOneApi>,
 }
@@ -123,13 +113,12 @@ impl CreditCardSupplier for SupplierOne {
     }
     fn from_input_init(&self, config: &str, property: &str) -> Box<dyn SubProductApi> {
         Box::new(SupplierOneApi {
-            fastcash_property: Some(property.to_string()),
-            fastcash_configs: Some(config.to_string()),
+            supplier_one_property: Some(property.to_string()),
+            supplier_one_configs: Some(config.to_string()),
         })
     }
 }
 
-// Implement PixProduct for Matera
 struct SupplierTwo {
     sub_product_api: Option<SupplierTwoApi>,
 }
@@ -152,14 +141,13 @@ impl CreditCardSupplier for SupplierTwo {
     }
     fn from_input_init(&self, config: &str, property: &str) -> Box<dyn SubProductApi> {
         Box::new(SupplierTwoApi {
-            matera_property: Some(property.to_string()),
-            matera_configs: Some(config.to_string()),
+            supplier_two_property: Some(property.to_string()),
+            supplier_two_configs: Some(config.to_string()),
         })
     }
 }
 
 fn main() {
-    // Example usage
 
     let mut pix = CreditCard::new();
     pix.set("SupplierOne");
